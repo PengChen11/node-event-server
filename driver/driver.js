8440fe144533274544965bc23be272308760dd09
Peng Chen <pchen0816@outlook.com>
2020-10-08 22:34:43 -0700

    done

---
+++ driver/driver.js
@@ -0,0 +1,23 @@
+'use strict';
+const io = require('socket.io-client');
+const host = 'http://localhost:3000/caps';
+
+const capsConnection = io.connect(host);
+
+capsConnection.on('pickup', onPickup);
+
+function onPickup(order) {
+  setTimeout(() => {
+
+    console.log(`pickup ${order.orderID}`);
+    capsConnection.emit('in-transit', order);
+
+  }, 1500);
+
+  setTimeout(()=>{
+    console.log(`delivered ${order.orderID}`);
+    capsConnection.emit('delivered', order);
+  }, 3000);
+}
+
+
