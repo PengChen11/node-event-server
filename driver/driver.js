7f27deaf609cf99f603e0ff70459dd6d12e6f087
Peng Chen <pchen0816@outlook.com>
2020-10-08 22:56:14 -0700

    deployed to heroku

--- driver/driver.js
+++ driver/driver.js
@@ -1,6 +1,7 @@
 'use strict';
 const io = require('socket.io-client');
-const host = 'http://localhost:3000/caps';
+// const host = 'http://localhost:3000/caps';
+const host = 'https://intense-lowlands-77850.herokuapp.com/caps';
 
 const capsConnection = io.connect(host);
 
